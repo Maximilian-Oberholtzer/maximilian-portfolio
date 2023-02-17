49b28cdd86fc5c58904ed56e8a9e9ebcf5e125fa
Maximilian Oberholtzer <max.oberholtzer@yahoo.com>
2023-02-16 21:55:41 -0500

    minor change to test safari scroller bug

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -27,6 +27,7 @@ const ScrollerIcon = styled("div")({
     transform: "translateX(-50%)",
     borderRadius: "50%",
     opacity: "1",
+    "-webkit-animation": "wheel 2s infinite",
     animation: "wheel 2s infinite",
   },
 });
