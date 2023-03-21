8fa1d8425e7b1c7c102b6125707b7cb141428a9e
Maximilian Oberholtzer <max.oberholtzer@yahoo.com>
2023-03-21 09:05:16 -0400

    added index file to organize component exports

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Loader } from "./Loader";
+export { default as Navbar } from "./Navbar";
+export { default as SideAnchorLinks } from "./SideAnchorLinks";
+export { default as Hero } from "./sections/Hero";
+export { default as About } from "./sections/About";
+export { default as Experience } from "./sections/Experience";
+export { default as Projects } from "./sections/Projects";
+export { default as Hobbies } from "./sections/Hobbies";
+export { default as Footer } from "./sections/Footer";
