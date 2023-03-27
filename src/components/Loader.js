4c399f4a65bff1502f11c3f4d61d76e9626ad67f
Maximilian Oberholtzer <max.oberholtzer@yahoo.com>
2023-03-27 11:58:01 -0400

    Improved accessibility for Navbar, style changes

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import { useTheme, useMediaQuery, styled } from "@mui/material";
+import { useTheme, styled } from "@mui/material";
 import { motion } from "framer-motion";
 import "animate.css";
 
